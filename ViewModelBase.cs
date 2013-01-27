02fe95a6a94c56080385f555073b58f344546801
Greg Wright <gregwwright@yahoo.co.uk>
2013-01-27 12:46:43 +0000

    Simple ViewModelbase
    Based on Mix talk from a few years ago

---
+++ ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace WPFStockTicker
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public void NotifyOfPropertyChange(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void NotifyOfPropertyChange<TProperty>(Expression<Func<TProperty>> property)
+        {
+            var lambda = (LambdaExpression)property;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else memberExpression = (MemberExpression)lambda.Body;
+
+            NotifyOfPropertyChange(memberExpression.Member.Name);
+        }
+    }
+}
